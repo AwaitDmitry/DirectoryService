b62e651eeaa522627bb2c9bc48a3d60d59139a5b
AwaitDmitry <moiseev28200405@gmail.com>
2026-06-29 19:27:11 +0400

    feat: подключена документация openapi, swaggerUI, созданы файлы directory: build и props

--- src/DirectoryService.Core/Program.cs
+++ src/DirectoryService.Core/Program.cs
@@ -1,39 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.Services.AddSwaggerGen(options =>
 {
-    app.MapOpenApi();
-}
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+        Title = "Directory Service",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "AwaitDmitry",
+        }
+    });
+});
+
+WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/test", () => "Hello World!");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
